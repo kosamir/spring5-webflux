d30828ba213cd08c8f78e4cb3cf163446eaba9de
Amir Kos <amir.kos@gmail.com>
2019-07-08 18:42:15 +0200

    prvi commit

---
+++ src/test/java/hr/optimus/spring5/webflux/Spring5WebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.optimus.spring5.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5WebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
